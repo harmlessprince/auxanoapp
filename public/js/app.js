f42d384a4bacd8d026ca850da6a9e312da831d03
harmlessprince <realolamilekan@gmail>
2022-03-19 20:34:59 +0100

    jjjd

--- public/js/app.js
+++ public/js/app.js
@@ -3914,17 +3914,15 @@ window._ = __webpack_require__(/*! lodash */ "./node_modules/lodash/lodash.js");
 
 window.axios = __webpack_require__(/*! axios */ "./node_modules/axios/index.js");
 window.axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
-window.axios.defaults.headers.common['Accept'] = 'application/json';
-var token = document.head.querySelector('meta[name="csrf-token"]');
+window.axios.defaults.headers.common['Accept'] = 'application/json'; // let token = document.head.querySelector('meta[name="csrf-token"]');
+// if (token) {
+//     window.csrfToken = document.querySelector('meta[name="csrf-token"]').content;
+//     window.axios.defaults.headers.common['X-CSRF-TOKEN'] = token.content;
+// } else {
+//     console.error('CSRF token not found: https://laravel.com/docs/csrf#csrf-x-csrf-token');
+// }
+// axios.defaults.withCredentials = true;
 
-if (token) {
-  window.csrfToken = document.querySelector('meta[name="csrf-token"]').content;
-  window.axios.defaults.headers.common['X-CSRF-TOKEN'] = token.content;
-} else {
-  console.error('CSRF token not found: https://laravel.com/docs/csrf#csrf-x-csrf-token');
-}
-
-axios.defaults.withCredentials = true;
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
  * for events that are broadcast by Laravel. Echo and event broadcasting
