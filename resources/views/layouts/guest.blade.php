bfba7dc39ac0d31ee826cca5f7168a71c7986908
harmlessprince <realolamilekan@gmail>
2022-03-19 16:23:53 +0100

    removed csrf from gues page

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -5,7 +5,6 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title> @yield('title') - {{ config('app.name') }}</title>
-    <meta name="csrf-token" content="{{ csrf_token() }}">
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap"
         rel="stylesheet" />
     <link rel="stylesheet" href="{{ asset('template/assets/css/tailwind.output.css') }}" />
