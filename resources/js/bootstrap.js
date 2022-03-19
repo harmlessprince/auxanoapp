4315d51a55f7c3f5a3a3eea9ea3c4fe061aeae3c
harmlessprince <realolamilekan@gmail>
2022-03-19 16:30:50 +0100

    debugging

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -10,8 +10,8 @@ window.axios = require('axios');
 
 axios.defaults.withCredentials = true;
 
-window.axios.defaults.headers.common['X-CSRF-TOKEN'] = window.Laravel.csrfToken;
-console.log(window.Laravel.csrfToken)
+window.axios.defaults.headers.common['X-CSRF-TOKEN'] =  document.querySelector('meta[name="csrf-token"]').getAttribute('content');
+
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
