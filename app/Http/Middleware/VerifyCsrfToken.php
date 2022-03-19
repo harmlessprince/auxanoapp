e289a9e6aadcf89e4b3cba82e4b6bc4f8e17627c
harmlessprince <realolamilekan@gmail>
2022-03-19 20:23:16 +0100

    off csrf

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -12,6 +12,7 @@ class VerifyCsrfToken extends Middleware
      * @var array<int, string>
      */
     protected $except = [
-        //
+        'http://auxanoapp.herokuapp.com',
+        'https://auxanoapp.herokuapp.com'
     ];
 }
