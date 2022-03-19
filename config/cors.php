8f7e189ed02ea28ed951a27fc65caf95e65130fa
harmlessprince <realolamilekan@gmail>
2022-03-19 15:55:10 +0100

    did somethih

--- config/cors.php
+++ config/cors.php
@@ -15,7 +15,10 @@ return [
     |
     */
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    'paths' => [
+        'api/*', '/login',
+        '/logout', 'sanctum/csrf-cookie'
+    ],
 
     'allowed_methods' => ['*'],
 
--- storage/framework/views/558d1047863395aedd9b25956f4a21ec8a6694a0.php
+++ storage/framework/views/558d1047863395aedd9b25956f4a21ec8a6694a0.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title> <?php echo $__env->yieldContent('title'); ?> - <?php echo e(config('app.name')); ?></title>
+    <meta name="csrf-token" content="<?php echo e(csrf_token()); ?>">
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap"
         rel="stylesheet" />
     <link rel="stylesheet" href="<?php echo e(asset('template/assets/css/tailwind.output.css')); ?>" />
