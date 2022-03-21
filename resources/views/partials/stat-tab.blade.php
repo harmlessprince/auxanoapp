cbeeebb02d8955ed3c2c647e8ea3d6f881b01f19
harmlessprince <realolamilekan@gmail>
2022-03-21 11:58:16 +0100

    changed header styling

--- resources/views/partials/stat-tab.blade.php
+++ resources/views/partials/stat-tab.blade.php
@@ -1,6 +1,6 @@
-<h4 class="mb-4 text-lg font-semibold text-gray-600 dark:text-gray-300">
+<h2 class="my-6 text-2xl font-semibold text-gray-700 dark:text-gray-200">
     Statistics Tab
-</h4>
+</h2>
 <div class="px-4 py-3 mb-8 bg-white rounded-lg shadow-md dark:bg-gray-800" id="app">
    <tab-component></tab-component>
 </div>
